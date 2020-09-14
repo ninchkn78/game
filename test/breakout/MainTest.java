9c0acb97ef2b52acc1fd0c24f1e81cd3794c36a1
Alex Chao <ac590@duke.edu>
2020-09-14 09:56:38 -0500

    ?

--- test/breakout/MainTest.java
+++ test/breakout/MainTest.java
@@ -10,7 +10,7 @@ import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
-
+//need to ask about how to adapt these tests for higher levels
 public class MainTest extends DukeApplicationTest {
 
   // create an instance of our game to be able to call in tests (like step())
