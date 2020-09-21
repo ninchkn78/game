5c260cfe19093d5f287cb9fb3987403c62eab98e
Christian Welch <cw356@duke.edu>
2020-09-21 15:04:32 -0500

    addedlives, loss condition, and add lives cheat key

--- src/breakout/Display.java
+++ src/breakout/Display.java
@@ -7,8 +7,8 @@ import javafx.scene.text.Text;
 
 public class Display {
 
-private int currentLives;
-private int currentScore;
+private int currentLives = 3;
+private int currentScore = 0;
 private Text stats;
 
     public Display(){
@@ -19,19 +19,38 @@ private Text stats;
 
 
     public Text createDisplay(){
+
        stats = new Text(350 / 2 - 50, 350 - 20, "Lives: " + currentLives + "     Score: " + currentScore);
        stats.setId("stats");
        return stats;
 
     }
-    public void updateScore(int score, Group root){
+    public void incrementScore(int score, Group root){
         root.getChildren().remove(stats);
-        this.currentScore = score;
+        this.currentScore += score;
+
         Text newStats = createDisplay();
         root.getChildren().add(newStats);
     }
+    public void decrementLives(int lives, Group root){
+        root.getChildren().remove(stats);
+        this.currentLives -= lives;
+        Text newStats = createDisplay();
+        root.getChildren().add(newStats);
+    }
+    /* might need this later? not sure
+    public void resetScore(Group root){
+        root.getChildren().remove(stats);
+        this.currentScore = 0;
+        Text newStats = createDisplay();
+        root.getChildren().add(newStats);
 
+    }
 
+     */
+    public int getLives(){
+        return currentLives;
+    }
 
 
 }
